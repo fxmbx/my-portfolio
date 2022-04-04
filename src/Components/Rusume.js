e2a07d754293d1495240734afe2b0bee56781612
fxmbx <funbiolaore@gmail.com>
2022-04-04 11:32:40 +0100

    vv

--- src/Components/Rusume.js
+++ src/Components/Rusume.js
@@ -1,9 +1,11 @@
-import React from 'react'
 
+
+import React from 'react'
+import Resume from '../assessts/Resume/Olaore_Oluwafunmibi Olumuyiwa_Resume_07-02-2022-17-01-08.pdf'
 function Rusume() {
     return (
 
-        <a className='resume' href='/assessts/Resume/Olaore_Oluwafunmibi Olumuyiwa_Resume_07-02-2022-17-01-08' target='_blank'>
+        <a className='resume' rel="noreferrer noreferrer" href={Resume} download={'Olaore_Oluwafunmibi Olumuyiwa_Resume_07-02-2022-17-01-08'} target='_blank'>
             <span className='resume-text'>
                 View my resume
             </span>
